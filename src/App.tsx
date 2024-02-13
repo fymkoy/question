5c90b0991d849e29e268900ba034c6aae891d094
fymkoy <kennethmariano2.0@gmail.com>
2024-02-13 21:38:36 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,21 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Sure kana dyan?",
+      "Sure na sure?",
+      "Sure na sure talaga?",
+      "Talagang talaga?",
+      "weh",
+      "Di nga?",
+      "Yung totoo",
+      "Bawal mag no",
+      "bawal nga kasi:((",
+      "please?",
+      "sige na logbi",
+      "kiss nalang kita",
+      "mwamwamwa",
+      "bawal pa din?",
+      "HEH WALA KA MAGAGAWA BLE",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">Ok yay!!!</div>
+          <div className="my-4 text-4xl font-bold">HEHEHE I LOVE YOU MWAMWA</div>
         </>
       ) : (
         <>
@@ -46,7 +46,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Will you be my Valentine bilog?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
